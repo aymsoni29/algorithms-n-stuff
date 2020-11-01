f90978301d33952f0a64974366c92a40096a771b
Abhay Soni <aymsoni29@gmail.com>
2020-11-01 10:16:38 -0800

    Linear Search

--- search/linear_search_test.go
+++ search/linear_search_test.go
@@ -1,4 +1,40 @@
 package search
 
-import "testing"
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
 
+func Test_LinearSearch(t *testing.T) {
+	testCases := []struct {
+		input          []int
+		target         int
+		expectedOutput bool
+	}{
+		{
+			input:          []int{1, 2, 3, 4},
+			target:         2,
+			expectedOutput: true,
+		},
+		{
+			input: []int{},
+			target: 2,
+			expectedOutput: false,
+		},
+		{
+			input: []int{1},
+			target: 2,
+			expectedOutput: false,
+		},
+		{
+			input: nil,
+			target: 2,
+			expectedOutput: false,
+		},
+	}
+
+	for _, tc := range(testCases) {
+		out := LinearSearch(tc.input, tc.target)
+		assert.Equal(t, tc.expectedOutput, out)
+	}
+}
