ff64aafe088834612be03d52986968ba0ad8ff0c
Abhay Soni <aymsoni29@gmail.com>
2020-11-15 18:46:45 -0800

    Init commit binary search

---
+++ search/binary_search.go
@@ -0,0 +1,7 @@
+package search
+
+func BinarySearch(input []int, target int) bool {
+	var lo, hi int = 0,0
+	
+	return false
+}
---
+++ search/binary_search_test.go
@@ -0,0 +1,40 @@
+package search
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_BinarySearch(t *testing.T) {
+	testCases := []struct {
+		input          []int
+		target         int
+		expectedOutput bool
+	}{
+		{
+			input:          []int{1, 2, 3, 4},
+			target:         2,
+			expectedOutput: true,
+		},
+		{
+			input: []int{},
+			target: 2,
+			expectedOutput: false,
+		},
+		{
+			input: []int{1},
+			target: 2,
+			expectedOutput: false,
+		},
+		{
+			input: nil,
+			target: 2,
+			expectedOutput: false,
+		},
+	}
+
+	for _, tc := range(testCases) {
+		out := BinarySearch(tc.input, tc.target)
+		assert.Equal(t, tc.expectedOutput, out)
+	}
+}
